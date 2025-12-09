8acd824178ef9edc86e43c83c3900fc49dacf41a
Fakhri Shafwan Malik <fakhri.xpl2@gmail.com>
2025-12-09 21:39:48 +0700

    feat:menambahkan fungsi khusus print message

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        printMessage();
+    }
+
+    private static void printMessage() {
+        System.out.println("Hello, World!");
     }
 }
+
